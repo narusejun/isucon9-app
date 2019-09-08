9a717b826bab8c155c27c397dee6811d7f952ea8
MonadicDavidHuang <mynameistoyoko@gmail.com>
2019-09-08 11:37:13 +0900

    Add pre-cacheing.

--- main.go
+++ main.go
@@ -525,6 +525,8 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 	json.NewEncoder(w).Encode(res)
+
+	getConfigByName("payment_service_url")
 }
 
 func getNewItems(w http.ResponseWriter, r *http.Request) {
